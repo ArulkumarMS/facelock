28a584fbb2c6cc69dae0a715ae9e2cdd4bcd1802
ShownX <dellsford@gmail.com>
2015-03-19 18:00:53 -0500

    Remove DerivedData and update

--- FaceLock/DerivedData/FaceLock/Build/Intermediates/FaceLock.build/Debug-iphonesimulator/FaceLock.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/LG/facelock/FaceLock/FaceLock/AppDelegate.m \
-  /Users/LG/facelock/FaceLock/FaceLock/AppDelegate.h
--- FaceLock/DerivedData/FaceLock/Build/Intermediates/FaceLock.build/Debug-iphonesimulator/FaceLock.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/LG/facelock/FaceLock/FaceLock/main.m \
-  /Users/LG/facelock/FaceLock/FaceLock/AppDelegate.h
