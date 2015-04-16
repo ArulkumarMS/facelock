bdf3d4b799fd04181cb4bdede99c6e0f84eee907
ShownX <dellsford@gmail.com>
2015-04-16 11:09:51 -0500

    Create 3D ImageView

---
+++ FaceLock/FaceLock/ViewController_3D.h
@@ -0,0 +1,13 @@
+//
+//  ViewController_3D.h
+//  FaceLock
+//
+//  Created by Alan Xu on 4/16/15.
+//  Copyright (c) 2015 CBL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController_3D : UIViewController
+
+@end
---
+++ FaceLock/FaceLock/ViewController_3D.mm
@@ -0,0 +1,37 @@
+//
+//  ViewController_3D.m
+//  FaceLock
+//
+//  Created by Alan Xu on 4/16/15.
+//  Copyright (c) 2015 CBL. All rights reserved.
+//
+
+#import "ViewController_3D.h"
+
+@interface ViewController_3D ()
+
+@end
+
+@implementation ViewController_3D
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
